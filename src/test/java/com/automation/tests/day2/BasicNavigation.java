fb23d2fc3091fbe31cc7dceffb121d7eee67ac2e
mehmetgenc85 <mancusun@gmail.com>
2020-04-01 18:08:06 -0400

    created basic navigation class

--- src/test/java/com/automation/tests/day2/BasicNavigation.java
+++ src/test/java/com/automation/tests/day2/BasicNavigation.java
@@ -16,6 +16,8 @@ public class BasicNavigation {
         // Chrome driver extends RemoteWebdriver ---> implements Webdriver
 
         driver.get("http://google.com"); // to open a website
+        driver.manage().window().maximize(); // to maximize browser
+        //driver.manage().window().fullscreen();
 
         Thread.sleep( 3000 ); // for demo, wait 3 seconds
 
@@ -29,11 +31,30 @@ public class BasicNavigation {
             System.out.println("TEST FAILED !");
         }
 
+        // go to another website within the same window
+
+        driver.navigate().to("http://amazon.com");
+
+        if(driver.getTitle().toLowerCase().contains("amazon")){
+            System.out.println("TEST PASSED !");
+        }else{
+            System.out.println("TEST FAILED !");
+        }
+
+
+       //MUST BE AT THE END
         driver.close(); // to close browser
 
         //browser cannot close itself
+    }
 
+    public static void verifyEquals(String arg1, String arg2){
 
+        if (arg1.equals(arg2)){
+            System.out.println("TEST PASSED !");
+        }else{
+            System.out.println("TEST FAILED !");
+        }
     }
 
 }
