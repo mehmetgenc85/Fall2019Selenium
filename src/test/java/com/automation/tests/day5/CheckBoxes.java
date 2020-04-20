d2f623d12df60a92fb981f7571e2ed661bc5a4eb
mehmetgenc85 <mancusun@gmail.com>
2020-04-19 20:24:58 -0400

    created basic navigation class

---
+++ src/test/java/com/automation/tests/day5/CheckBoxes.java
@@ -0,0 +1,38 @@
+package com.automation.tests.day5;
+
+import com.automation.utilities.BrowserUtils;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.List;
+public class CheckBoxes {
+
+    public static void main(String[] args) {
+
+        WebDriverManager.chromedriver().version("81").setup();
+        WebDriver driver= new ChromeDriver();
+        driver.get("http://practice.cybertekschool.com/checkboxes");
+        BrowserUtils.wait(2);
+
+        List<WebElement> checkBoxes = driver.findElements(By.tagName("input"));
+
+        checkBoxes.get(0).click(); // click on first checkbox
+
+        BrowserUtils.wait(2);
+
+        checkBoxes.get(1).click(); // click on the second checkbox
+
+        BrowserUtils.wait(2);
+
+
+
+
+
+
+        driver.quit();
+
+    }
+}
