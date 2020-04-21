ead7ed1535f4366dbbdf75a11ccea8e2f5738011
mehmetgenc85 <mancusun@gmail.com>
2020-04-21 18:09:12 -0400

    created basic navigation class

--- src/test/java/com/automation/tests/day5/TestForiFRAME.java
+++ src/test/java/com/automation/tests/day5/TestForiFRAME.java
@@ -2,7 +2,9 @@ package com.automation.tests.day5;
 
 import com.automation.utilities.BrowserUtils;
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class TestForiFRAME {
@@ -13,7 +15,28 @@ public class TestForiFRAME {
         WebDriver driver= new ChromeDriver();
         driver.get("http://practice.cybertekschool.com/iframe");
         BrowserUtils.wait(4);
+        // before looking for that element, we need to jump that frame
+        // you can specify: name, id, index or webelement of the frame
+        // it s like we are jumping another layer
+        driver.switchTo().frame("mce_0_ifr");
 
+        // now this content will be visible
+        WebElement textInput = driver.findElement(By.id("tinymce"));
+
+        System.out.println(textInput.getText());
+        BrowserUtils.wait(4);
+
+        textInput.clear(); // to delete text
+        textInput.sendKeys("Hello, World!");
+
+        BrowserUtils.wait(4);
+
+
+        // exit from the frame
+        driver.switchTo().defaultContent();
+
+        WebElement heading = driver.findElement(By.tagName("h3"));
+        System.out.println(heading.getText());
 
         driver.quit();
 
