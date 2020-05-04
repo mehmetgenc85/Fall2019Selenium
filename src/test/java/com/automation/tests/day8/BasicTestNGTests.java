cc72d461360aec358ad79c790d8beda4f9b8b836
mehmetgenc85 <mancusun@gmail.com>
2020-05-04 00:20:40 -0400

    day 7: Xpath

--- src/test/java/com/automation/tests/day8/BasicTestNGTests.java
+++ src/test/java/com/automation/tests/day8/BasicTestNGTests.java
@@ -1,4 +1,28 @@
 package com.automation.tests.day8;
 
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
 public class BasicTestNGTests {
+
+
+    @Test
+    public void test1(){
+        System.out.println("TEST 1");
+        String expected = "apple";
+        String actual = "apple";
+        Assert.assertEquals(actual,expected);
+
+    }
+    @Test
+    public void test2(){
+        System.out.println("TEST 2");
+        int num1 = 5;
+        int num2 =10;
+        // it calls hard assertion.
+        // if assertion fails - it stops the execution (due to exception)
+        Assert.assertTrue(num1 > num2);
+
+
+    }
 }
